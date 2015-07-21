0c8d0dbaa9a50bef69617c5a1771978b7fae338b
Duy Luong <luongvd@Duy-Luong.local>
2015-07-21 23:06:07 +0700

    Import Resource

---
+++ Tuition/Tuition/Controller/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  Tuition
+//
+//  Created by Duy Luong on 7/21/15.
+//  Copyright (c) 2015 Duy Luong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
---
+++ Tuition/Tuition/Controller/SplashViewController.h
@@ -0,0 +1,13 @@
+//
+//  SplashViewController.h
+//  Tuition
+//
+//  Created by Duy Luong on 7/21/15.
+//  Copyright (c) 2015 Duy Luong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SplashViewController : UIViewController
+
+@end
