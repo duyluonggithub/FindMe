660b77976303313728a02324bdaf2ed9298e9670
Duy Luong <luongvd@Duy-Luong.local>
2015-07-21 22:01:41 +0700

    Init Source

---
+++ Tuition/Tuition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tuition
+//
+//  Created by Duy Luong on 7/21/15.
+//  Copyright (c) 2015 Duy Luong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tuition/Tuition/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tuition
+//
+//  Created by Duy Luong on 7/21/15.
+//  Copyright (c) 2015 Duy Luong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
