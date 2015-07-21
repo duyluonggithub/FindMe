faadc78ca643bb385e222242b9fbebb3a5a1b37b
Duy Luong <luongvd@Duy-Luong.local>
2015-07-22 01:50:10 +0700

    - Update : Splash screen, About screen

---
+++ Tuition/Tuition/Controller/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  Tuition
+//
+//  Created by Duy Luong on 7/21/15.
+//  Copyright (c) 2015 Duy Luong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
